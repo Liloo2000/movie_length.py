c8fbede38e62e76edd0468ccd970d6151d7f36dd
Liloo2000 <132087712+Liloo2000@users.noreply.github.com>
2023-04-29 18:12:48 +0100

    Update movie_length.py

--- movie_length.py
+++ movie_length.py
@@ -47,9 +47,11 @@ class LongestMovies(MRJob):
 	def mapper2(self, movie_id, avg_rating):
 		yield None, (avg_rating, movie_id)
 	
-	def reducer2(self, _, values):
-		print(values)
+	def reducer2(self, key, values):
+		values = list(values)
+		print(key.decode())
 		for movie_id, (movie_title, title_len) in sorted(values, key=lambda x: -int(x[1])):
+			print(f'{movie_title.decode()} ({title_len.decode()} min)')
 			i += 1
 		if i <= self.SHOW_LIMIT:
 			yield movie_id, (movie_title, title_len)
