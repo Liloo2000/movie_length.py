d5a246140d7cd563779465124156125491284c0b
Liloo2000 <132087712+Liloo2000@users.noreply.github.com>
2023-04-29 17:41:19 +0100

    Update movie_length.py

--- movie_length.py
+++ movie_length.py
@@ -48,7 +48,7 @@ class LongestMovies(MRJob):
 		yield None, (avg_rating, movie_id)
 	
 	def reducer2(self, _, values):
-		for movie_id, (movie_title, title_len) in sorted(values, key=lambda x: -x[1]):
+		for movie_id, (movie_title, title_len) in sorted(values, key=lambda x: -int(x[1])):
 			i += 1
 		if i <= self.SHOW_LIMIT:
 			yield movie_id, (movie_title, title_len)
